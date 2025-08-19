00d6e2a473b7bb8ef5493a3f0c9e9cbafc218f62
drelephant <drelephant@nope>
2025-08-20 00:25:49 +1000

    update version number

--- stan_nospoiler.js
+++ stan_nospoiler.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Stan Nospoiler
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.2
 // @description  Toggle Timeline
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
@@ -14,7 +14,7 @@
 
 (function() {
 
-    console.log("Stan Toggle Timeline v1.1 userscript started!");
+    console.log("Stan Toggle Timeline v1.2 userscript started!");
 
     let video;
 
